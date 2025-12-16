948fce779a6a9948aa1af6b75f9805e2ada22eab
kenny0023 <hei.bryan.2@gmail.com>
2025-12-16 12:37:45 +0100

    un petit changement de code

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
-    private static final String USER = "mini_dish_db_manager";
-    private static final String PASSWORD = "1234";
+    private final String URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
+    private final String USER = "mini_dish_db_manager";
+    private final String PASSWORD = "1234";
 
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
